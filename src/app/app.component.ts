91aa445aae93af2ffccd6545b34f0a44b59512f1
Samuel <blas.rayon.samuel@gmail.com>
2023-01-15 18:12:47 -0600

    consultando ingredientes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'recipies';
+  urlIngredients = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
+  populares = [];
+  constructor(public http: HttpClient) {}
+  ngOnInit(): void {
+    let ingrediPopulares = Array.apply(null, { length: 10 }).map((x) =>
+      Math.round(Math.random() * 100)
+    );
+
+    this.http
+      .get(this.urlIngredients, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Headers': 'Content-Type',
+        },
+      })
+      .subscribe((data) => {
+        this.populares = ingrediPopulares.map(function (item, index) {
+          return data.meals[item];
+        });
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { CardComponent } from './card/card.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
